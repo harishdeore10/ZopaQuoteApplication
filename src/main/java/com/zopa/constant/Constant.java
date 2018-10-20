312adc5696b7ae3b9c6430c3bd5fd2e3614e5764
harishdeore10 <harishdeore10@gmail.com>
2018-10-20 15:59:30 +0100

    Added files required for project structure

---
+++ src/main/java/com/zopa/constant/Constant.java
@@ -0,0 +1,9 @@
+package com.zopa.constant;
+
+public class Constant {
+    public static final Integer LOAN_LENGTH = 36;
+    public static final String AMOUNT_TOO_HIGH_OR_LOW = "Loan amount not accepted. " +
+            "Please ensure you have entered a number rounded to the nearest hundred between £1,000 and £150,000";
+    public static final String INVALID_INPUT = "Sorry, you have entered an invalid input.\n" +
+            "Please ensure you have provided a CSV file as the first parameter and a loan amount as the second parameter";
+}
